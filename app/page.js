2d94f25d9e3a79bb8ff687cfa683287fee3d735d
Santiago Arias <santi99oca@gmail.com>
2024-01-19 12:48:56 -0300

    Update background color and text color in NextUIProvider

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ import Registers from "./Registers";
 
 export default function Home() {
   return (
-    <NextUIProvider className="h-screen flex justify-center w-screen">
+    <NextUIProvider className="h-screen flex justify-center w-screen bg-black text-white">
       <GlobalContextProvider>
         <div className="flex flex-col items-center gap-2 p-4 max-w-[768px] w-full">
           <h1 className="self-stretch text-center text-2xl line leading-7 weight font-normal">
