9c7aca2911fe2205ad33576e4bfb1c86946b1691
Santiago Arias <santi99oca@gmail.com>
2024-01-19 17:24:20 -0300

    Update NextUIProvider class in Home component

--- app/page.js
+++ app/page.js
@@ -7,14 +7,16 @@ import Registers from "./Registers";
 
 export default function Home() {
   return (
-    <NextUIProvider className="h-screen flex justify-center w-screen bg-black text-white">
+    <NextUIProvider className="h-screen flex justify-center w-screen bg-black text-white dark">
       <GlobalContextProvider>
         <div className="flex flex-col items-center gap-2 p-4 max-w-[768px] w-full">
           <h1 className="self-stretch text-center text-2xl line leading-7 weight font-normal">
             Flash Cards Comu
           </h1>
           <Top />
+
           <Card />
+
           <Registers />
         </div>
       </GlobalContextProvider>
